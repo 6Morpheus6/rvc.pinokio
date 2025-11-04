0148c84ed7faa29d31397f17ff457525a1fc7699
Morpheus <127767180+6Morpheus6@users.noreply.github.com>
2025-11-04 04:36:45 +0100

    update huggingface_hub
    updating huggingface_hub in the base environment is crucial since hf download didn't exist yet in earlier huggingface_hub version ~ huggingface_hub<0.30

--- install.js
+++ install.js
@@ -9,6 +9,14 @@ module.exports = {
       }
     },
     {
+      method: "shell.run",
+      params: {
+        message: [
+          "conda update -y -c conda-forge huggingface_hub",
+        ]
+      }
+    },
+    {
       method: "script.start",
       params: {
         uri: "torch.js",
